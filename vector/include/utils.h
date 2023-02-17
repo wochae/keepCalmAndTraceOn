c237721e11a77a05967771227e9ada747e7d6199
Wooseok Chae <wochae@cx1r1s1.42seoul.kr>
2023-02-17 21:16:29 +0900

    mod dir utils add vec3_utils

--- vector/include/utils.h
+++ vector/include/utils.h
@@ -16,4 +16,13 @@ t_vec3 vplus(t_vec3 vec, t_vec3 vec2);
 t_vec3 vplus_(t_vec3 vec, double x, double y, double z);
 t_vec3 vminus(t_vec3 vec, t_vec3 vec2);
 t_vec3 vminus_(t_vec3 vec, t_vec3 vec2);
-t_vec3 v
+t_vec3 vmult(t_vec3 vec, double t);
+t_vec3 vmult_(t_vec3 vec, t_vec3 vec2);
+t_vec3 vdivide(t_vec3 vec, double t);
+t_vec3 vdivide_(t_vec3 vec, t_vec3 vec2);
+double vdot(t_vec3 vec, t_vec3 vec2);
+t_vec3 vcross(t_vec3 vec, t_vec3 vec2);
+t_vec3 vunit(t_vec3 vec);
+t_vec3 vmin(t_vec3 vec1, t_vec3 vec2);
+
+#endif
