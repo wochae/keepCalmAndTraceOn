cd11c54c8afe35ed6c14e9b06c494e8f98fc1a0f
wochae <wochae@cx2r1s2.42seoul.kr>
2023-02-21 16:26:21 +0900

    [fix] : modified types to formed by tuto

--- miniRT/headers/structures.h
+++ miniRT/headers/structures.h
@@ -6,17 +6,6 @@
 typedef struct s_vec3		t_vec3;
 typedef struct s_vec3		t_point3;
 typedef struct s_vec3		t_color3;
-typedef struct s_ray		t_ray;
-typedef struct s_hit_record	t_hit_record;
-
-typedef struct s_camera		t_camera;
-typedef struct s_canvas		t_canvas;
-typedef struct s_scene		t_scene;
-
-typedef struct s_object		t_object;
-typedef struct s_sphere		t_sphere;
-typedef struct s_light		t_light;
-
 typedef int					t_bool;
 # define FALSE 0
 # define TRUE 1
@@ -35,13 +24,14 @@ struct s_vec3
 	double	z;
 };
 
-struct	s_ray
+typedef struct s_ambient
 {
-	t_point3	orig;
-	t_vec3		dir;
-};
+	t_color3	color;
+	double		ratio;
+}	t_ambient;
 
-struct s_hit_record
+
+typedef struct s_hit_record
 {
 	t_point3	p;
 	t_vec3		normal;
@@ -50,22 +40,9 @@ struct s_hit_record
 	double		tmax;
 	double		t;
 	t_color3	albedo;
-};
+}	t_hit_record;
 
-
-/* 장면 구조체
-struct	s_camera
-{
-	t_point3		orig;  // 카메라 원점(위치)
-	double			viewport_h; // 뷰포트 세로길이
-	double			viewport_w; // 뷰포트 가로길이
-	t_vec3			horizontal; // 수평길이 벡터
-	t_vec3			vertical; // 수직길이 벡터
-	double			focal_len; // focal length
-	t_point3		left_bottom; // 왼쪽 아래 코너점
-};
-*/
-struct	s_camera
+typedef struct s_camera
 {
 	t_point3		orig;
 	t_point3		left_bottom;
@@ -74,16 +51,16 @@ struct	s_camera
 	double			viewport_w;
 	double			viewport_h;
 	double			focal_len;
-};
+}	t_camera;
 
-struct	s_canvas
+typedef struct s_canvas
 {
 	int		width;
 	int		height;
 	double	aspect_ratio;
-};
+}	t_canvas;
 
-struct	s_scene
+typedef struct s_scene
 {
 	t_canvas		canvas;
 	t_camera		camera;
@@ -91,29 +68,98 @@ struct	s_scene
 	t_object		*light;
 	t_ray			ray;
 	t_hit_record	rec;
-};
+}	t_scene;
 
 // 오브젝트 구조체
-struct	s_object
+typedef struct s_object
 {
 	t_object_type	type;
 	void			*element;
 	void			*next;
 	t_color3		albedo;
-};
+}	t_object;
 
-struct	s_sphere
-{
-	t_point3	center;
-	double		radius;
-	double		radius2;
-};
-
-struct	s_light
+typedef struct s_light
 {
 	t_point3		origin;
 	t_color3		light_color;
 	double			bright_ratio;
-};
+}	t_light;
+
+typedef struct s_sphere
+{
+	t_point3	center;
+	t_color3	color;
+	double		radius;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	t_point3	pos;
+	t_vec3		dir;
+	t_color3	color;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	t_point3	pos;
+	t_vec3		dir;
+	t_color3	color;
+	double		r;
+	double		height;
+}	t_cylinder;
+
+typedef struct s_obj
+{
+	int				type;
+	void			*obj_info;
+}	t_obj;
+
+typedef struct s_hit_check
+{
+	t_point3	pos;
+	t_vec3		n_vec;
+	t_color3	albedo;
+	double		t;
+	double		t_min;
+	double		t_max;
+}	t_hit_check;
+
+typedef struct s_ray
+{
+	t_point3	orig;
+	t_vec3		dir;
+}	t_ray;
+
+typedef struct s_pong
+{
+	t_vec3		lig_dir;
+	t_vec3		view_dir;
+	t_vec3		ref_dir;
+	t_vec3		dif;
+	t_vec3		specular;
+	double		spec;
+	double		ks;
+	double		ksn;
+	double		kd;
+	double		lig_len;
+}	t_pong;
+
+typedef struct s_info
+{
+	void				*mlx;
+	void				*win;
+	void				*img;
+	char				*addr;
+	int					bpp;
+	int					size_line;
+	int					endian;
+	struct s_ambient	amb;
+	t_camera			cam;
+	struct s_light		light;
+	void				*clicked;
+	int					clicked_type;
+	t_list				*objs;
+}	t_info;
 
 #endif
