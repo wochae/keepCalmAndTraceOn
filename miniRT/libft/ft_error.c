58793907f799f71d7d111fc93e476719645dcc99
wochae <officialwochae@gmail.com>
2023-02-23 18:07:49 +0900

    add colors.h ft_error in lib

---
+++ miniRT/libft/ft_error.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+#include "colors.h"
+
+void    ft_error(char *msg)
+{
+    ft_putstr_fd(RED, 2);
+    ft_putendl_fd("Error", 2);
+    ft_putstr_fd(YELLOW, 2);
+    ft_putendl_fd(msg, 2);
+    exit(1);
+}
